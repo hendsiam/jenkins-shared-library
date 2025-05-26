af42af796272d65d9c690bb09a446aa0764dcd97
ehab salamaa <127217918+hendsiam@users.noreply.github.com>
2025-05-26 22:53:37 +0100

    Update pipelineLib2.groovy

--- vars/pipelineLib2.groovy
+++ vars/pipelineLib2.groovy
@@ -1,24 +1,24 @@
-pipeline {
-    agent any
+def call() {
+    pipeline {
+        agent any
 
-    environment {
-        XYZ = 'ITI ITI ITI'
-        IMAGE_NAME = 'hendsiam/jenkins'
-    }
+        environment {
+            XYZ = 'ITI ITI ITI'
+            IMAGE_NAME = 'hendsiam/jenkins'
 
-    stages {
-        stage('Build & Dockerize') {
-            steps {
-                echo 'Building Docker image for Python app...'
-                sh "docker build -t ${IMAGE_NAME}:latest ."
+
+        stages {
+            stage('Build & Dockerize') {
+                steps {
+                    echo 'Building Docker image for Python app...'
+                    sh 'docker build -t my-python-app .'
+                }
             }
-        }
-        stage('Push Docker Image') {
-            steps {
-                echo 'Pushing Docker image to DockerHub'
-                withCredentials([usernamePassword(credentialsId: 'dockerhub-creds', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
-                    sh "echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin"
-                    sh "docker push ${IMAGE_NAME}:latest"
+
+            stage('Push Docker Image') {
+                steps {
+                    echo 'Pushing Docker image to DockerHub'
+                    pushDockerImage(env.IMAGE_NAME)
                 }
             }
         }
