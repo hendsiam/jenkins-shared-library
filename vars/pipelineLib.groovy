d9f2fe0e608041faf68bccede5eee38a8923d676
hendsiam <hendsiam13@gmail.com>
2025-05-26 13:45:30 +0100

    Initial commit for Jenkins shared library

---
+++ vars/pipelineLib.groovy
@@ -0,0 +1,41 @@
+def buildDockerImage() {
+    echo "🔨 Building Docker image..."
+    def imageName = "hendsiam/jenkins"
+    sh "docker build -t ${imageName}:v${env.BUILD_NUMBER} ."
+}
+
+def pushDockerImage() {
+    echo "🚀 Pushing Docker image to DockerHub..."
+    def imageName = "hendsiam/jenkins"
+    withCredentials([usernamePassword(credentialsId: 'dockerhub-cred', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
+        sh "echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin"
+        sh "docker push ${imageName}:v${env.BUILD_NUMBER}"
+    }
+}
+
+def buildAndPushDockerImage() {
+    pipeline {
+        agent any
+
+        environment {
+            XYZ = 'ITI ITI ITI'
+        }
+
+        stages {
+            stage('Build Docker Image') {
+                steps {
+                    script {
+                        buildDockerImage()
+                    }
+                }
+            }
+            stage('Push Docker Image') {
+                steps {
+                    script {
+                        pushDockerImage()
+                    }
+                }
+            }
+        }
+    }
+}
