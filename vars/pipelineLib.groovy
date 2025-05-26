febd9be9343a7dfa16401d7b26584dafc4bb7489
ehab salamaa <127217918+hendsiam@users.noreply.github.com>
2025-05-26 14:52:55 +0100

    Update pipelineLib.groovy

--- vars/pipelineLib.groovy
+++ vars/pipelineLib.groovy
@@ -8,34 +8,13 @@ def pushDockerImage() {
     echo "🚀 Pushing Docker image to DockerHub..."
     def imageName = "hendsiam/jenkins"
     withCredentials([usernamePassword(credentialsId: 'dockerhub-cred', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
-        sh "echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin"
+        sh "echo \$DOCKER_PASSWORD | docker login -u \$DOCKER_USERNAME --password-stdin"
         sh "docker push ${imageName}:v${env.BUILD_NUMBER}"
     }
 }
 
 def buildAndPushDockerImage() {
-    pipeline {
-        agent any
-
-        environment {
-            XYZ = 'ITI ITI ITI'
-        }
-
-        stages {
-            stage('Build Docker Image') {
-                steps {
-                    script {
-                        buildDockerImage()
-                    }
-                }
-            }
-            stage('Push Docker Image') {
-                steps {
-                    script {
-                        pushDockerImage()
-                    }
-                }
-            }
-        }
-    }
+    buildDockerImage()
+    pushDockerImage()
 }
+
